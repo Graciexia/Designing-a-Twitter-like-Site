4413ca13f8d34d88d317f75377a350e3a83cd461
xiawei <graciexia@outlook.com>
2015-06-10 20:54:39 -0500

    initialize3

--- db/migrate/20150611013420_create_tweets.rb
+++ db/migrate/20150611013420_create_tweets.rb
@@ -2,6 +2,7 @@ class CreateTweets < ActiveRecord::Migration
   def change
     create_table :tweets do |t|
       t.string :messages
+      t.integer :user_id
 
       t.timestamps null: false
     end
